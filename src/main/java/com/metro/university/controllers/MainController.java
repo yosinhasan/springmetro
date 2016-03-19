fb9b5ae2f988a701ad60a68d501ad4d540efc1f5
Yosin <yosinhasan@gmail.com>
2016-03-19 13:35:51 +0200

    fix logging bug, add hibernate configuration

--- src/main/java/com/metro/university/controllers/MainController.java
+++ src/main/java/com/metro/university/controllers/MainController.java
@@ -6,10 +6,14 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.metro.university.dao.impl.FilesDAOImpl;
+import com.metro.university.entity.FilesEntity;
 import org.apache.log4j.Logger;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,43 +22,48 @@ import com.metro.university.utils.ReadFile;
 import com.metro.university.utils.TotalMark;
 
 /**
- * 
  * @author elpai
- *
  */
+@EnableTransactionManagement
 @Controller
 public class MainController {
-	private static final Logger LOG = Logger.getLogger(MainController.class);
-	@Autowired
-	private MessageSource messageSource;
+    private static final Logger LOG = Logger.getLogger(MainController.class);
+    @Autowired
+    private MessageSource messageSource;
+    @Autowired
+    FilesDAOImpl filesDAOImpl;
 
-	@RequestMapping("/process")
-	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		LOG.debug("Main controller: action process");
-		LOG.debug("Action started");
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("process");
-		LOG.trace("Set request parameter: pieDataList ");
-		String path = request.getServletContext().getRealPath("/files/112625.dat");
-		LOG.trace("Path: " + path);
-		ReadFile rf = new ReadFile(new File(path));
-		List<IntervalChart.KeyValue> vert = IntervalChart.getVertical(rf.getDataEntities());
-		List<IntervalChart.KeyValue> hor = IntervalChart.getHorizontal(rf.getDataEntities());
-		LOG.trace("Set request parameter: vertical");
-		mv.addObject("vertical", vert);
-		LOG.trace("Set request parameter: horizontal");
-		mv.addObject("horizontal", hor);
-		TotalMark marks = new TotalMark(rf.getPicketEntities(), rf.getDataEntities());
-		List<String> result = marks.getResult();
-		LOG.trace("Set request parameter: marks");
-		mv.addObject("marks", result);
-		LOG.debug("Action finished");
-		return mv;
-	}
 
-	@RequestMapping("/welcome")
-	public ModelAndView welcome(HttpServletRequest request, HttpServletResponse response) {
-
-		return new ModelAndView("welcome");
-	}
+    @RequestMapping("/process")
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        LOG.debug("Main controller: action process");
+        LOG.debug("Action started");
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("process");
+        LOG.trace("Set request parameter: pieDataList ");
+        String path = request.getServletContext().getRealPath("/files/112625.dat");
+        LOG.trace("Path: " + path);
+        ReadFile rf = new ReadFile(new File(path));
+        List<IntervalChart.KeyValue> vert = IntervalChart.getVertical(rf.getDataEntities());
+        List<IntervalChart.KeyValue> hor = IntervalChart.getHorizontal(rf.getDataEntities());
+        LOG.trace("Set request parameter: vertical");
+        mv.addObject("vertical", vert);
+        LOG.trace("Set request parameter: horizontal");
+        mv.addObject("horizontal", hor);
+        TotalMark marks = new TotalMark(rf.getPicketEntities(), rf.getDataEntities());
+        List<String> result = marks.getResult();
+        LOG.trace("Set request parameter: marks");
+        mv.addObject("marks", result);
+        LOG.debug("Action finished");
+        return mv;
+    }
+    @RequestMapping("/welcome")
+    public ModelAndView welcome(HttpServletRequest request, HttpServletResponse response) {
+        LOG.debug("Main controller: action welcome");
+        LOG.debug("Action started");
+        List<FilesEntity> list = filesDAOImpl.getFiles();
+        LOG.trace("Found in db: files = " + list);
+        LOG.debug("Action finished");
+        return new ModelAndView("welcome");
+    }
 }
--- src/main/java/com/metro/university/entity/FilesEntity.java
+++ src/main/java/com/metro/university/entity/FilesEntity.java
@@ -42,4 +42,8 @@ public class FilesEntity {
         result = 31 * result + (name != null ? name.hashCode() : 0);
         return result;
     }
+    @Override
+    public String toString() {
+        return name;
+    }
 }
